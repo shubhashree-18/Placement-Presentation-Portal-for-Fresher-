24ddb5517891baef92dfe3f3a2718688b0c9a0e5
Shubhashree M <shubhashree73506@gmail.com>
2026-02-03 11:31:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function startTest(type) {
+    alert(type + " Test will be available soon!\nKeep preparing 👍");
+}
+
+function showTips() {
+    alert(
+        "HR Interview Tips:\n\n" +
+        "1. Be confident\n" +
+        "2. Speak clearly\n" +
+        "3. Know your resume well\n" +
+        "4. Be honest\n"
+    );
+}
+
+function startMock() {
+    alert(
+        "Mock Test Started!\n\n" +
+        "Sections:\n" +
+        "• Aptitude\n" +
+        "• Technical\n" +
+        "• HR\n"
+    );
+}
